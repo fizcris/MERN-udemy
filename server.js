ef3a6b735bf650e11762a512127a997a89020dbc
fizcris <fizcris@gmail.com>
2019-08-14 19:54:08 +0100

    API routes created

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ connectDB();
 
 app.get("/", (req, res) => res.send("API running"));
 
+// Define routes
+
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/post", require("./routes/api/post"));
+app.use("/api/profile", require("./routes/api/profile"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on Port: ${PORT}`));

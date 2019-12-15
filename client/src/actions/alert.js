7f787d85ad933dce76dc70fc09493459f7ad89d1
Cristian Alonso <fizcris@gmail.com>
2019-12-15 14:32:48 +0100

    Alert included

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,10 +1,12 @@
 import { SET_ALERT, REMOVE_ALERT } from "./types";
-import uuid from "uuid";
+import uuid from "uuid"; // Generates an universal id on the fly
 
-export const setAlert = (msg, alertType) => dispatch => {
+export const setAlert = (msg, alertType, timeOut = 2000) => dispatch => {
   const id = uuid.v4();
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id }
   });
+
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeOut);
 };

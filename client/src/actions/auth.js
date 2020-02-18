c19517e023d3a9f4d811be285b3d09f4636e5bfe
Cristian Alonso <fizcris@gmail.com>
2020-02-18 18:49:08 +0100

    Oauth fully implemented

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -3,8 +3,11 @@ import { setAlert } from "./alert";
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
   USER_LOADED,
-  AUTH_ERROR
+  AUTH_ERROR,
+  LOGOUT
 } from "./types";
 import setAuthToken from "../utils/setAuthToken";
 // Load User
@@ -42,6 +45,7 @@ export const register = ({ name, email, password }) => async dispatch => {
       type: REGISTER_SUCCESS,
       payload: res.data
     });
+    dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
 
@@ -52,4 +56,38 @@ export const register = ({ name, email, password }) => async dispatch => {
       type: REGISTER_FAIL
     });
   }
+};
+
+// Log in User
+export const login = (email, password) => async dispatch => {
+  const config = {
+    headers: { "Content-type": "application/json" }
+  };
+
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.post("api/auth", body, config);
+
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data
+    });
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+    }
+    dispatch({
+      type: LOGIN_FAIL
+    });
+  }
+};
+
+// Logout / Clear profile
+
+export const logout = () => dispatch => {
+  dispatch({ type: LOGOUT });
 };

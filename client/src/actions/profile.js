63f11d44739dc69c74f02837e39adfbb064b1a37
Cristian Alonso <fizcris@gmail.com>
2020-03-18 17:55:44 +0100

    Create Profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -14,6 +14,54 @@ export const getCurrentProfile = () => async dispatch => {
       payload: res.data
     });
   } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status,
+        data: err.response.data
+      }
+    });
+  }
+};
+
+//Create or update user profile
+
+export const createProfile = (
+  formData,
+  history,
+  edit = false
+) => async dispatch => {
+  try {
+    const config = {
+      headers: { "Content-Type": "application/json" }
+    };
+
+    const res = await axios.post("/api/profile", formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert(edit ? "Profile Updated" : "Profile created", "succes"));
+
+    if (!edit) {
+      history.push("./dashboard");
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+    }
+
     dispatch({
       type: PROFILE_ERROR,
       payload: {
